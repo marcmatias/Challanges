baba14582633ed2972eade8570fbd6c4307a9d5e
Marcel Marques <marcmatiasdev@gmail.com>
2021-05-04 11:58:02 -0300

    New challange edabit python

---
+++ Edabit/python/Are the Numbers Equal.py
@@ -0,0 +1,14 @@
+'''
+
+    Are the Numbers Equal?
+
+    Create a function that returns True when num1 is equal to num2; 
+    Otherwise return False
+
+'''
+
+
+def is_same_num(num1, num2):
+	return num1 is num2 and num1 == num2
+
+print(is_same_num(4, 8))
